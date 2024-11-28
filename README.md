eb1525d2a4ddd65160001c34c2b775e15d56c741
Sherwin <87555304+owenlim225@users.noreply.github.com>
2024-11-29 01:36:26 +0800

    select pokemon # ⚠️ Untested
    ASHASHSAHSAHA ayaw gumana pre. check mo nlng, thankss <3

--- README.md
+++ README.md
@@ -4,9 +4,9 @@
 
 Dev notes:
 '''
-# 🟧 in progress
-# ✅ working
-# ⚠️ Untested
+# 🟧 in progress [di pa tapos isulat yung buong method]
+# ✅ working [good nato. tapos na yung method at na test nadin. gumagana lahat.]
+# ⚠️ Untested [done writing method, tinatamad lang icheck kung nagana SHASHASHASH]
 
 '''
 
--- frontend.py
+++ frontend.py
@@ -1,3 +1,17 @@
 # 🟧 in progress
 # ✅ working
 # ⚠️ Untested
+
+
+
+from Backend import Backend
+class Frontend:
+    def __init__(self) -> None:
+        self.backend = Backend()
+
+    # 🟧 in progress
+    def run(self) -> None:
+        while len(self.backend.player1_pokemons) < 3 and len(self.backend.player2_pokemons) < 3:
+            
+            self.backend.select_pokemon(self.backend.player1_pokemons, "Player 1")
+            self.backend.select_pokemon(self.backend.player2_pokemons, "Player 2")
