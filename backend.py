688f865f5a5b08783feb65d53c94762d1869cb6a
Ellisia <christianjudevillaber@gmail.com>
2024-11-30 11:34:15 +0800

    backend.select pokemon: 🟧 in progress

--- backend.py
+++ backend.py
@@ -14,14 +14,14 @@ class Backend:
         self.player2_pokemons = Linked_List()
         self.pokemon_array = Pokemon_Array()
 
-    # ⚠️ Untested Add.Note: ASHSAHASHSAHASH di na dedelete amp
-    def select_pokemon(self, player, player_num) -> None:
+    # 🟧 in progress | note: Linked list in progress
+    def select_pokemon(self, player: Linked_List, player_num_str: str) -> None:
         # Display the available Pokémon
         self.pokemon_array.show_pokemons()
 
         try:
             # Prompt the player to select a Pokémon by its index
-            player_choice = int(input(f"{player_num}, enter the index of the Pokémon you want to select: "))
+            player_choice = int(input(f"{player_num_str}, enter the index of the Pokémon you want to select: "))
 
             # Get the selected Pokémon and remove it from the array
             selected_pokemon = self.pokemon_array.select_and_remove_pokemon(player_choice)
@@ -29,25 +29,20 @@ class Backend:
             # Add the chosen Pokémon to the player's linked list
             if selected_pokemon:
                 player.insert_at_end(selected_pokemon[0])  # Use the name of the Pokémon
-                print(f"{player_num}'s Pokémon: ", player.print_linked_list())
+                print(f"{player_num_str}'s Pokémon: ", player.print_linked_list())
             else:
                 print("Invalid selection. Try again.")
-
-            
+ 
         except (ValueError, IndexError):
             print("Invalid input. Please enter a valid index.")
 
 
 if __name__ == "__main__":
     backend = Backend()
-    pokemon_array = Pokemon_Array()
-
-    pokemon_array.show_pokemons()
     print("Pick your pokemon.\n")
 
     backend.select_pokemon(backend.player1_pokemons, "Player 1")
-    print(f"\n\ndapat wala na si Pikachu")
-    pokemon_array.show_pokemons()
+    print( backend.pokemon_array.show_pokemons()) #backend.pokemon_array
 
 
 
