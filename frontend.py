b6b4a04a748c7949fb6281aae6cefe1301bbd419
Ellisia <christianjudevillaber@gmail.com>
2024-11-30 12:29:35 +0800

    test commit

--- frontend.py
+++ frontend.py
@@ -10,7 +10,12 @@ class Frontend:
 
     # 🟧 in progress
     def run(self) -> None:
-        while len(self.backend.player1_pokemons) < 3 and len(self.backend.player2_pokemons) < 3:
+        while self.backend.player1_pokemons.size_of_linked_list() < 3 and self.backend.player2_pokemons.size_of_linked_list() < 3:
             
             self.backend.select_pokemon(self.backend.player1_pokemons, "Player 1")
             self.backend.select_pokemon(self.backend.player2_pokemons, "Player 2")
+
+
+if __name__ == "__main__":
+    frontend = Frontend()
+    frontend.run()
